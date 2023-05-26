9b80e0aef34e0b6cb7b317378b7b61063df12ef0
Sholpan Zhamali <sholpan.zhamali@gmail.com>
2023-05-26 14:37:04 +0600

    resources routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-   # Defines the root path route ("/")
-   root "cars#index"
+  root "cars#index"
 
-   get "/cars", to: "cars#index"
-    get "/cars/:id", to: "cars#show"
-   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :cars
 end
