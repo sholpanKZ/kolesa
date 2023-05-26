3e8b9a545757f3c14c797596e6f1f6dd42743634
Sholpan Zhamali <sholpan.zhamali@gmail.com>
2023-05-26 15:27:29 +0600

    validation and errors for creating a new car

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,10 @@
 class Car < ApplicationRecord
 
+  validates :name, presence: true
+  validates :price, presence: true,  length: { minimum: 5 }
+  validates :description, presence: true,  length: { minimum: 20 }
+  validates :color, presence: true
+
   VALID_COLORS = ['white', 'black', 'blue', 'grey', 'brown', 'red']
 
   validates :color, inclusion: { in: VALID_COLORS }
