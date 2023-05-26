0c6d124d41cea07aabfca6601f58f4158aace4d7
Sholpan Zhamali <sholpan.zhamali@gmail.com>
2023-05-26 13:12:57 +0600

    root route to Cars#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-   get "/cars", to: "cars#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+   # Defines the root path route ("/")
+   root "cars#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+   get "/cars", to: "cars#index"
+   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
