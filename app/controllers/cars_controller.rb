bdb2f4b32c53055cf78ca4b5874959e48ed217e8
Sholpan Zhamali <sholpan.zhamali@gmail.com>
2023-05-26 15:01:10 +0600

    creat new car

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -6,4 +6,18 @@ class CarsController < ApplicationController
   def show
     @car = Car.find(params[:id])
   end
+
+   def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(name: "...", price: 0, description: "...", color: "...")
+
+    if @car.save
+      redirect_to @car
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
