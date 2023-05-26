3ae100f6b6dfa4a1327a2d88cd19469454d4f2f9
Sholpan Zhamali <sholpan.zhamali@gmail.com>
2023-05-26 16:03:23 +0600

    update cars

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -14,10 +14,25 @@ class CarsController < ApplicationController
   def create
     @car = Car.new(car_params)
 
+
+
     if @car.save
       redirect_to @car
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+ def edit
+   @car = Car.find(params[:id])
+  end
+
+  def update
+    @car = Car.find(params[:id])
+
+    if @car.update(car_params)
+      redirect_to @car
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
