afdb14535224f831112517f91c6fa0be6123728d
Sholpan Zhamali <sholpan.zhamali@gmail.com>
2023-05-29 14:56:07 +0600

    added action destroy for delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
     redirect_to car_path(@car)
   end
 
+   def destroy
+    @car = Car.find(params[:car_id])
+    @comment = @car.comments.find(params[:id])
+    @comment.destroy
+    redirect_to car_path(@car), status: :see_other
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body, :status)
